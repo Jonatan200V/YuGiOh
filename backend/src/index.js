892f6add109263128cbcba4505ee5a4389104fb4
Jonatan <jonatanangel88@gmail.com>
2022-09-18 23:01:01 -0300

    Guardado

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const data = require("../data/data");
 const fs = require("fs");
+const bodyParser = require("body-parser");
 // const logger = require("morgan");
 const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 // app.use(logger());
+app.use(bodyParser.json());
 
 app.get("/api/yugioh", (req, res) => {
   res.json(data);
@@ -19,7 +21,11 @@ app.get("/api/yugioh/:id", (req, res, next) => {
   }
   res.status(404).send("PAGE NOT FOUND");
 });
-
+app.post("/api/yugioh", (req, res) => {
+  const card = { id: data.length + 1, ...req.body };
+  data = [...data, card];
+  res.status(201).json(data);
+});
 app.use((req, res, next) => {
   fs.readFile(`index.html`, "utf-8", (err, content) => {
     if (err) {
